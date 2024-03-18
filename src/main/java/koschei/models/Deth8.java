59c7b816a6271463cc145709dc766dda4ee559e2
Alexander Smith <mr.alexander.smith@hotmail.com>
2024-03-18 14:57:29 +0300

    Task is done.

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,15 @@
 package koschei.models;
 
+import org.springframework.stereotype.Component;
+
+/**
+ * В класс Deth8 внесены изменения в связи неправильной постановкой задачи.
+ * <br>Описания проблемы и её решения см. в этом методе {@link Needle7#toString()}.</br>
+ * */
+@Component
 public class Deth8 {
+    @Override
+    public String toString() {
+        return ":(";
+    }
 }
--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,9 +1,25 @@
 package koschei.models;
 
-public class Needle7 {
+import org.springframework.stereotype.Component;
 
+@Component
+public class Needle7 {
+    private final Deth8 deth = new Deth8();
+
+    /**
+     * Задача составлена неправильно:
+     * <p><code {@code }>return ", смерть Кощея на игле :( " + "";</code></p>
+     * <ul><b><u>Логичное продолжение:</u></b>
+     *     <li>создать метод toString() в классе Deth8;</li>
+     *     <li>переместить смайлик в строку возврата метода toString класса Deth8;</li>
+     *     <li>добавить приватное поле 'Deth8 deth' в класс Needle7;</li>
+     *     <li>добавить создание бина от класса Deth8;</li>
+     *     <li>внедрить зависимость от Deth8 в класс Needle7;</li>
+     *     <li>добавить в строку возврата метода toString класса Needle7 вызов deth.toString().</li>
+     * </ul>
+     * */
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+        return ", смерть Кощея на игле " + deth.toString();
     }
 }
